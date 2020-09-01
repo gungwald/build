9ea259b98604a5d863d0018456dc990c7a2b1960
Bill Chatfield <bill_chatfield@yahoo.com>
2020-09-01 10:03:26 -0400

    Improved exception message

--- src/com/alteredmechanism/apps/build/ProcessFailureException.java
+++ src/com/alteredmechanism/apps/build/ProcessFailureException.java
@@ -4,8 +4,18 @@ public class ProcessFailureException extends Exception {
     public String[] cmd;
     public int exitCode;
     public ProcessFailureException(String[] cmd, int exitCode) {
-        super("Process failed with exit code " + exitCode);
+        super("Process failed with exit code: " + exitCode + ": " + join(" ", cmd));
         this.cmd = cmd;
         this.exitCode = exitCode;
+    }
+
+    protected static String join(String separator, String[] items) {
+        StringBuffer joined = new StringBuffer();
+        for (int i = 0; i < items.length; i++) {
+            joined.append(items[i]);
+            joined.append(separator);
+        }
+        joined.setLength(joined.length() - separator.length());
+        return joined.toString();
     }
 }
